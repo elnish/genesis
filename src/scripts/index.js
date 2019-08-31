a291d599acad60ec25b8df9066241c60f112d43f
elnish <a.shobotova@hotmail.com>
2019-08-31 12:42:16 +0300

    570px w

---
+++ src/scripts/index.js
@@ -0,0 +1,10 @@
+// // change gender active btn
+// let genders = document.querySelectorAll('.aside__input-box')[0];
+// let btns = genders.getElementsByClassName('aside__form-input_short');
+// for (let i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener('click', function() {
+// //   let current = genders.getElementsByClassName('aside__form-input');
+//   this.classList.toggle('aside__form-input_active');
+// //   current[0].className = current[0].className.replace(' aside__form-input_active', '');
+//   });
+// }
