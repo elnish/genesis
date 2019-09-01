2985d5e78b9290c9274c267f1faa68cab493f010
elnish <a.shobotova@hotmail.com>
2019-09-01 20:16:01 +0300

    form validation

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -31,8 +31,7 @@ selections(variants, 'aside__variant_active', variant => {
     orangeBtn.classList.remove('hidden');
  
     if (variant == 0) {
-        resultInner("10%", "A");
-        
+        resultInner("10%", "A");   
     } else if (variant == 1) {
         resultInner("30%", "Б");
     } else {
@@ -40,9 +39,6 @@ selections(variants, 'aside__variant_active', variant => {
     }
     
 });
-
-
-
 
 
 // Slider
@@ -65,8 +61,14 @@ next.forEach(elt => elt.addEventListener('click', plusSlides, false));
 
 
 
+//Validation
+let form = document.querySelector('[data-form]');
+let name = document.querySelector('[data-name]');
+let email = document.querySelector('[data-email]');
+let password = document.querySelector('[data-password]');
+let emailRegex = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+let passwordRegex = /.{8,}/;
 
-//form checkbox
 let checkbox = document.querySelector('[data-checkbox]');
 let submitBtn = document.querySelector('[data-submit-btn]');
 
@@ -80,52 +82,60 @@ checkbox.addEventListener('click', function () {
 })
 
 
-//Validation
-let form = document.querySelector('[data-form]');
-let name = document.querySelector('[data-name]');
-let email = document.querySelector('[data-email]');
-let password = document.querySelector('[data-password]');
-let btn = document.querySelector('[data-submit-btn]');
-let emailRegex = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-let passwordRegex = /.{8,}/;
-
 function errorMessage(el, message) {
     el.placeholder = message;
     el.classList.add('aside__form-input_error');
+    checkbox.checked = false;
+    submitBtn.setAttribute('disabled', true);
+}
+
+function clear(el) {
+    el.classList.remove('aside__form-input_error');
 }
 
 
-selections(genders, 'aside__form-input_active', variant => console.log(variant));
+selections(genders, 'aside__form-input_active', variant => console.log (variant));
 
 function validate(event) {
     event.preventDefault();
-    //name
-    if (name.value.length == 0) {
-        errorMessage(name, 'Введите свое имя');
-        // onfocus(name);  
+
+    //gender
+    if (!genders[0].classList.contains('aside__form-input_active') && !genders[1].classList.contains('aside__form-input_active')) {
+        genders.forEach(gender => gender.classList.add('aside__form-input_error')); 
     } else {
-        successMessage(name);
+        genders.forEach(gender => gender.classList.remove('aside__form-input_error'));
     }
-    //email
+
+    // name
+    if (name.value.length == 0) {
+        errorMessage(name, 'Введите свое имя'); 
+    } else {
+        clear(name);
+    }
+
+    // email
     if (email.value.length == 0) {
         errorMessage(email, 'Введите свой email');
-        // onfocus(email);
-        
+        errors = 1;
     } else if (!emailRegex.test(email.value)) {
         errorMessage(email, 'Неверный формат email');
-        // onfocus(email);
-    } 
+        errors = 1;
+    } else {
+        clear(email);
+    }
+
     //password
     if (password.value.length == 0) {
         errorMessage(password, 'Придумайте новый пароль');
-        // onfocus(password);
+        errors = 1;
     } else if (!passwordRegex.test(password.value)) {
         errorMessage(password, 'Паль должен содержать не меньше 8 знаков');
-        // onfocus(password);
+        errors = 1;
     } else {
-        successMessage(password);
+        clear(password);
     }
 }
+
 
 form.addEventListener('submit', validate);
 })();
